7b007e2fc4cc451aec2788e57d1df1a4efb0c628
durgeshkumar18 <durgeshkumar18>
2026-07-19 13:15:39 +0530

    Added First Basic  Program

---
+++ Basics/First BAsic Program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("=========================");
+    printf("\n C PROGRAMMING BASICS \n");
+    printf("=========================");
+    printf("Name:Durgesh Kumar Nishad\n");
+    printf("Roll No: 2300560310018\n");
+    printf("Course: B.Tech\n");
+    printf("Branch: Electronic and Communication Engineering\n");
+    printf("College: BAbu Bamarasi Das National Institute of Technology, Lucknow\n");
+    printf("Goal: Embedded Systems Engineer\n");
+    printf("==========================");
+    return 0;
+}
