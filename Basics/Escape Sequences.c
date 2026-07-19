35160340c10fe255d705998e7bb5ecb56685b997
durgeshkumar18 <durgeshkumar18>
2026-07-19 12:59:20 +0530

    Added Escape sequences Program

---
+++ Basics/Escape Sequences.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("New Line Example\n");
+    printf("Hello\tWorld!\n\n");
+    printf("Tab Example:\n");
+    printf("Name\tAge\tCity\n");
+    printf("Durgesh\t20\tLucknow\n");
+    printf("Backslash: \\\n");
+    printf("Double Quote: \"C Programming\"\n");
+    return 0;
+}
